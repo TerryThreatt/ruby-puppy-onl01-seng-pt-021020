2d757d7a8001dec87206fc123ba4de8a2a6cb220
Terry Threatt <terry.threatt@gmail.com>
2020-03-02 18:54:04 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,28 @@
 # Add your code here
+
+class Dog 
+  
+  @@all = []
+  
+  attr_accessor :name 
+  
+  def initialize(name)
+    @name = name
+  end 
+  
+  def self.all
+    @@all
+  end 
+  
+  def self.print_all
+    @@all.each {|dog| puts dog.name}
+  end 
+  
+  def self.clear_all
+    @@all.clear
+  end 
+  
+  def save(new_dog)
+    @@all << new_dog
+  end 
+end 
